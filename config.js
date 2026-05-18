c164726fecacbbb8147dd29a7cb5efb17eb3107c
C. M. Lin <bagilu@gmail.com>
2026-05-18 14:48:37 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 注意：這裡只能放公開資訊，不能放 LiveKit API Secret。
 window.P104_CONFIG = {
   LIVEKIT_URL: "wss://test-r0pww5a8.livekit.cloud",
-  TOKEN_FUNCTION_URL: "https://mfljkyvdadxlrbxlboce.supabase.co/functions/v1/P104_livekit_token"
+  TOKEN_FUNCTION_URL: "https://mfljkyvdadxlrbxlboce.supabase.co/functions/P104_livekit_token"
 };
